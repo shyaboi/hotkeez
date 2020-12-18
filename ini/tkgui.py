7a39dc2ffdbfd0b0b6375652243f60a3bde6d90d
Shyaboi <ian.shane.sears@gmail.com>
2020-12-17 23:03:55 -0700

    low trans warning made

--- ini/tkgui.py
+++ ini/tkgui.py
@@ -1,22 +1,41 @@
 from tkinter import *
-
+import tkinter as tk
+from tkinter import messagebox as mb
 # Create object
 root = Tk()
 
 # Adjust size
-root.geometry("400x400")
+root.geometry("800x200")
 
-def print_value(val):
+def getValue(val):
     # w2.set(88)
+    if int(val) <= 33:
+        if int(val) <= 33:
+            mb.showerror("Answer", "If you go lower you might not be able to see HotKeez :(")
     w2.set(val)
     roundy = int(val)/100
     print(roundy)
     root.attributes('-alpha', roundy)
 
 
-# Create transparent window
-root.attributes('-alpha',0.5)
-w2 = Scale(root, from_=0, to=100, length=600,tickinterval=10, orient=HORIZONTAL, command=print_value)
+
+
+# tk.Button(text='Answer', command=answer).pack(fill=tk.X)
+leftFrame = Frame(root)
+leftFrame.pack(side=LEFT)
+ 
+rightFrame = Frame(root)
+rightFrame.pack(side=RIGHT)
+ 
+label1 = Label(leftFrame, text="Left column")
+label1.pack()
+ 
+label3 = Label(leftFrame, text="Column content")
+label3.pack()
+ 
+label2 = Label(rightFrame, text="Right column")
+label2.pack()
+w2 = Scale(root, from_=33, to=100, length=600,tickinterval=10, orient=HORIZONTAL, command=getValue)
 w2.set(88)
 w2Lable = Label(root, text='Transparentcy')
 w2.pack()
