02094fcac6ccd09e249db98cfdf06eec2e432f7c
Shyaboi <ian.shane.sears@gmail.com>
2020-12-18 22:47:40 -0700

    1st clipboard copying to gui

--- ini/tkgui.py
+++ ini/tkgui.py
@@ -5,7 +5,7 @@ from tkinter import messagebox as mb
 root = Tk()
 
 # Adjust size
-root.geometry("800x200")
+root.geometry("1250x200")
 
 def getValue(val):
     # w2.set(88)
@@ -17,24 +17,27 @@ def getValue(val):
     print(roundy)
     root.attributes('-alpha', roundy)
 
+def setHotKee(event):
+    ok = root.clipboard_get()
+    print(ok, event)
+    hotkee1.config(text=ok)
 
+def key_press(event):
+    key = event.char
+    print(key, 'is pressed')
 
-
+root.bind('<Key>', setHotKee)
 # tk.Button(text='Answer', command=answer).pack(fill=tk.X)
 leftFrame = Frame(root)
 leftFrame.pack(side=LEFT)
  
-rightFrame = Frame(root)
-rightFrame.pack(side=RIGHT)
- 
+
 label1 = Label(leftFrame, text="Left column")
 label1.pack()
  
 label3 = Label(leftFrame, text="Column content")
 label3.pack()
- 
-label2 = Label(rightFrame, text="Right column")
-label2.pack()
+
 
 
 w2Lable = Label(root, text='Transparentcy')
@@ -42,18 +45,29 @@ w2Lable.pack(side=TOP)
 w2 = Scale(root, from_=33, to=100, length=600,tickinterval=10, orient=HORIZONTAL, command=getValue)
 w2.set(88)
 w2.pack()
-label2 = Label(root, text="HotKeez 1", borderwidth=9, relief="ridge", cursor='trek', height=7, width=20)
-label2.pack(side=LEFT,padx=10)
-label2 = Label(root, text="HotKeez 2", borderwidth=9, relief="ridge", cursor='trek', height=7, width=20)
-label2.pack(side=LEFT,padx=10)
-label2 = Label(root, text="HotKeez 3", borderwidth=9, relief="ridge", cursor='trek', height=7, width=20)
-label2.pack(side=LEFT,padx=10)
-label2 = Label(root, text="HotKeez 4", borderwidth=9, relief="ridge", cursor='trek', height=7, width=20)
-label2.pack(side=LEFT,padx=10)
-label2 = Label(root, text="HotKeez 5", borderwidth=9, relief="ridge", cursor='trek', height=7, width=20)
-label2.pack(side=LEFT,padx=10)
-label2 = Label(root, text="HotKeez 6", borderwidth=9, relief="ridge", cursor='trek', height=7, width=20)
-label2.pack(side=LEFT,padx=10)
+hotkee1 = Label(root, text="HotKee 1", borderwidth=9, relief="ridge", cursor='trek', height=7, width=20)
+hotkee1.pack(side=LEFT,padx=10)
+
+
+hotkee2 = Label(root, text="HotKee 2", borderwidth=9, relief="ridge", cursor='trek', height=7, width=20)
+hotkee2.pack(side=LEFT,padx=10)
+
+
+hotkee3 = Label(root, text="HotKee 3", borderwidth=9, relief="ridge", cursor='trek', height=7, width=20)
+hotkee3.pack(side=LEFT,padx=10)
+
+
+hotkee4 = Label(root, text="HotKee 4", borderwidth=9, relief="ridge", cursor='trek', height=7, width=20)
+hotkee4.pack(side=LEFT,padx=10)
+
+
+hotkee5 = Label(root, text="HotKee 5", borderwidth=9, relief="ridge", cursor='trek', height=7, width=20)
+hotkee5.pack(side=LEFT,padx=10)
+
+
+hotkee6 = Label(root, text="HotKee 6", borderwidth=9, relief="ridge", cursor='trek', height=7, width=20)
+hotkee6.pack(side=LEFT,padx=10)
 # Execute tkinter always on top
 root.call('wm', 'attributes', '.', '-topmost', '1')
+
 root.mainloop()
