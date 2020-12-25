fd9b8834e544a0e18cf6425b841da6f4ca9ffcc8
Shyaboi <ian.shane.sears@gmail.com>
2020-12-24 22:55:28 -0700

    getting chat of keypress, and copying to clipboard with pyautogui and pyperclip

--- ini/keez.py
+++ ini/keez.py
@@ -1,14 +1,29 @@
-from pynput.keyboard import Key, Listener
+from pynput.keyboard import Key, Listener,KeyCode
 import threading
+import sys
+import pyautogui as pya
+import pyperclip
+import time
+
+
+def copy_clipboard():
+    pya.hotkey('ctrl', 'c')
+    time.sleep(.01)  # ctrl-c is usually very fast but your program may execute faster
+    return pyperclip.paste()    
 
 
 def on_press(key):
-    print('{0} pressed'.format(
-        key))
-
+    print(key)
+    
 def on_release(key):
-    print('{0} release'.format(
-        key))
+    # print('{0} release'.format(key.char))
+    
+    # if key.format(key.char) == 'a':
+    #     
+    if key == KeyCode(vk = 0, char='1'):
+        clippy = copy_clipboard()
+        print(clippy)
+        
     if key == Key.esc:
         # Stop listener
         return False
