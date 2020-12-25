e15290ec6232f9f2587085846570cacf25d27a03
Shyaboi <ian.shane.sears@gmail.com>
2020-12-24 23:48:19 -0700

    copy and paste mostly working

--- ini/keez.py
+++ ini/keez.py
@@ -5,12 +5,29 @@ import pyautogui as pya
 import pyperclip
 import time
 
+hotKeez = []
+
+def get_capslock_state():
+    import ctypes
+    hllDll = ctypes.WinDLL ("User32.dll")
+    VK_CAPITAL = 0x14
+    return hllDll.GetKeyState(VK_CAPITAL)
+
+def get_scrolllock_state():
+    import ctypes
+    hllDll = ctypes.WinDLL ("User32.dll")
+    VK_SCROLL = 0x91
+    return hllDll.GetKeyState(VK_SCROLL)
 
 def copy_clipboard():
     pya.hotkey('ctrl', 'c')
     time.sleep(.01)  # ctrl-c is usually very fast but your program may execute faster
     return pyperclip.paste()    
 
+def paste_clipboard():
+    pya.press('backspace')
+    pya.hotkey('ctrl', 'v')
+    time.sleep(.01)  # ctrl-c is usually very fast but your program may execute faster
 
 def on_press(key):
     print(key)
@@ -20,10 +37,13 @@ def on_release(key):
     
     # if key.format(key.char) == 'a':
     #     
+
     if key == KeyCode(vk = 0, char='1'):
-        clippy = copy_clipboard()
-        print(clippy)
-        
+        copy_clipboard()
+        print(get_scrolllock_state())
+    if key == KeyCode(vk = 0, char='2'):
+        paste_clipboard()
+   
     if key == Key.esc:
         # Stop listener
         return False
